3d0d149beaa51d1c6b8d1e27d26c2e6e73dd74d1
Izatis <mekinovizat1@gmail.com>
2023-05-11 22:29:10 +0600

    font

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { FC } from "react";
 import s from "./Order.module.scss";
+import Image from "next/image";
+import { Data } from "../Orders/Orders";
 
-const Order = () => {
-    return (
+interface OrderProps {
+  order: Data;
+}
+
+const Order: FC<OrderProps> = ({ order }) => {
+  console.log(order);
+
+  return (
+    <div className={s.order}>
+      <input type="checkbox" />
+      <div className={s.order__info}>
+        <Image
+          className={s.order__image}
+          src={order.imageFirst}
+          alt="order image"
+        />
         <div>
-            
+          <h1>{order.title}</h1>
+          <h2>{order.fullname}</h2>
         </div>
-    );
+      </div>
+      <p>{order.price}</p>
+      <div className={s.order__info}>
+        <Image src={order.imageSecond} alt="order image" />
+        <p>{order.price}</p>
+      </div>
+      <span>{order.date}</span>
+      <span>X</span>
+    </div>
+  );
 };
 
 export default Order;
--- src/components/Orders/Orders.tsx
+++ src/components/Orders/Orders.tsx
@@ -1,15 +1,123 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 import s from "./Orders.module.scss";
+import Order from "../Order/Order";
+import Image from "next/image";
+
+import imageFirst from "../../../public/Mark.svg";
+import imageSecond from "../../../public/Mark.svg";
+import imageThree from "../../../public/Mark.svg";
+
+export interface Data {
+  title: string;
+  fullname: string;
+  date: string;
+  price: string;
+  imageFirst: string;
+  imageSecond: string;
+}
 
 const Orders: FC = () => {
-    return (
-        <div className={s.orders}>
-            <h1 className={s.orders__title}>Active Bids</h1>
-            <div>
-                <input type="checkbox" />
-            </div>
+  const data: Data[] = [
+    {
+      title: "Cute Cube Cool",
+      fullname: "John Abraham",
+      date: "2 Hours 1 min 30s	",
+      price: "0.0025 ETH",
+      imageFirst: require("../../../public/NFT.png"),
+      imageSecond: require("../../../public/order.png"),
+    },
+    // {
+    //   title: "Liquid Wave",
+    //   fullname: "John Abraham",
+    //   date: "2 Hours 1 min 30s	",
+    //   price: 0.0025,
+    //   image: "../../../public/NFT (1).png",
+    // },
+    // {
+    //   title: "Cute Cube Cool",
+    //   fullname: "John Abraham",
+    //   date: "2 Hours 1 min 30s	",
+    //   price: 0.0025,
+    //   image: "../../../public/NFT (2).png",
+    // },
+    // {
+    //   title: "Liquid Wave",
+    //   fullname: "John Abraham",
+    //   date: "2 Hours 1 min 30s	",
+    //   price: 0.0025,
+    //   image: "../../../public/NFT (3).png",
+    // },
+    // {
+    //   title: "Liquid Wave",
+    //   fullname: "John Abraham",
+    //   date: "2 Hours 1 min 30s	",
+    //   price: 0.0025,
+    //   image: "../../../public/NFT (4).png",
+    // },
+  ];
+
+  return (
+    <div className={s.basket}>
+
+      <div className={s.bids}>
+
+        <h1>Bids</h1>
+        <h2>Welcome Bids Page</h2>
+
+       <div className={s.bidsCards}>
+       <div className={s.bidsCard}>
+          <Image src={imageFirst} alt="Mark" />
+          <div className={s.bidsCard__text}>
+            <h1>24K</h1>
+            <p>Artworks</p>
+          </div>
         </div>
-    );
+
+        <div className={s.bidsCard}>
+          <Image src={imageFirst} alt="Mark" />
+          <div className={s.bidsCard__text}>
+            <h1>24K</h1>
+            <p>Artworks</p>
+          </div>
+        </div>
+
+        <div className={s.bidsCard}>
+          <Image src={imageFirst} alt="Mark" />
+          <div className={s.bidsCard__text}>
+            <h1>24K</h1>
+            <p>Artworks</p>
+          </div>
+        </div>
+
+        <div className={s.bidsCard}>
+          <Image src={imageFirst} alt="Mark" />
+          <div className={s.bidsCard__text}>
+            <h1>24K</h1>
+            <p>Artworks</p>
+          </div>
+        </div>
+       </div>
+
+      </div>
+
+      <div className={s.orders}>
+        <h1 className={s.orders__title}>Active Bids</h1>
+        <div>
+          <ul className={s.orders__description}>
+            <input type="checkbox" />
+            <li>Item List</li>
+            <li>Open Price</li>
+            <li>Recent Offer</li>
+            <li>Time Left</li>
+            <li>Action</li>
+          </ul>
+          {data.map((item: Data) => {
+            return <Order order={item} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
